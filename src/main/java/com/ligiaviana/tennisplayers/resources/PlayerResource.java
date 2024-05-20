eeb24cd350b73f331f066125b59108bdd3fc5a52
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-20 10:19:08 -0300

    Service layer, component registration

--- src/main/java/com/ligiaviana/tennisplayers/resources/PlayerResource.java
+++ src/main/java/com/ligiaviana/tennisplayers/resources/PlayerResource.java
@@ -1,20 +1,35 @@
 package com.ligiaviana.tennisplayers.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ligiaviana.tennisplayers.entities.Player;
+import com.ligiaviana.tennisplayers.services.PlayerService;
 
 @RestController
 @RequestMapping(value ="/players")
 public class PlayerResource {
 	
+	@Autowired
+	private PlayerService service;
+	
 	@GetMapping
-	public ResponseEntity<Player> findAll(){
-		Player p = new Player(1L, "Novak Djokovic");
-		return ResponseEntity.ok().body(p);
+	public ResponseEntity<List<Player>> findAll(){
+		List<Player> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Player> findById(@PathVariable Long id) {
+		Player obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 
 }
---
+++ src/main/java/com/ligiaviana/tennisplayers/services/PlayerService.java
@@ -0,0 +1,27 @@
+package com.ligiaviana.tennisplayers.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ligiaviana.tennisplayers.entities.Player;
+import com.ligiaviana.tennisplayers.repositories.PlayerRepository;
+
+@Service
+public class PlayerService {
+	
+	@Autowired 
+	private PlayerRepository repository;
+	
+	public List<Player> findAll() {
+		return repository.findAll();
+	}
+	
+	public Player findById(Long id) {
+		Optional<Player> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
