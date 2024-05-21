fb771ace3a9290ca0dede44b58139d38bfe23c9d
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-21 13:44:22 -0300

    Exception handling - delete

--- src/main/java/com/ligiaviana/tennisplayers/services/PlayerService.java
+++ src/main/java/com/ligiaviana/tennisplayers/services/PlayerService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ligiaviana.tennisplayers.entities.Player;
 import com.ligiaviana.tennisplayers.repositories.PlayerRepository;
+import com.ligiaviana.tennisplayers.services.exceptions.DatabaseException;
 import com.ligiaviana.tennisplayers.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class PlayerService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public Player update(Long id, Player obj) {
